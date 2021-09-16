a05acee3d3faf8f37326e81d7a6b6dfa2133070a
ThoNohT <e.c.p.bataille@gmail.com>
2021-09-16 22:19:37 +0200

    Properly propagate state in the loop

--- src/Exy.hs
+++ src/Exy.hs
@@ -1,27 +1,23 @@
 module Exy (run) where
 
-import Prelude hiding (until)
-
 import Control.Monad.State.Lazy
 import System.IO
+import Prelude hiding (until)
 
-until :: Monad m => (a -> Bool) -> m a -> m a
-until break m = do
-    res <- m
-    if break res then pure res else until break m
+data Output = Continue | Quite
 
 run :: IO ()
-run = do
-    _ <- until ("quit" ==) $ evalStateT step 0
-    pure ()
-
+run = run' 0
+  where
+    run' st = do
+      (out, st') <- runStateT step st
+      if out == "quit" then pure () else run' st'
 
 step :: StateT Int IO String
 step = do
-    liftIO $ putStrLn "Type something..."
-    x <- liftIO getLine
-    y <- get
-    liftIO $ print y
-    put $ y + 1
-    pure x
-
+  liftIO $ putStrLn "Type something..."
+  x <- liftIO getLine
+  y <- get
+  liftIO $ print y
+  put $ y + 1
+  pure x
