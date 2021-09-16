442347da7451389ee498aeecb539fec8b8068869
ThoNohT <e.c.p.bataille@gmail.com>
2021-09-16 22:00:15 +0200

    Introduce state monad
    However, it cannot yet store state since it is reinitialized every time.

--- src/Exy.hs
+++ src/Exy.hs
@@ -1,6 +1,8 @@
 module Exy (run) where
 
 import Prelude hiding (until)
+
+import Control.Monad.State.Lazy
 import System.IO
 
 until :: Monad m => (a -> Bool) -> m a -> m a
@@ -10,8 +12,16 @@ until break m = do
 
 run :: IO ()
 run = do
-    _ <- until (== "quit") $ do
-            putStrLn "Type something..."
-            getLine
+    _ <- until ("quit" ==) $ evalStateT step 0
     pure ()
+
+
+step :: StateT Int IO String
+step = do
+    liftIO $ putStrLn "Type something..."
+    x <- liftIO getLine
+    y <- get
+    liftIO $ print y
+    put $ y + 1
+    pure x
 
