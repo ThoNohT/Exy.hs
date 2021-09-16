3575943c938417c86bbc27ae8a90658ef5d3d044
ThoNohT <e.c.p.bataille@gmail.com>
2021-09-16 21:43:12 +0200

    Create an input loop

--- src/Exy.hs
+++ src/Exy.hs
@@ -1,4 +1,17 @@
 module Exy (run) where
 
+import Prelude hiding (until)
+import System.IO
+
+until :: Monad m => (a -> Bool) -> m a -> m a
+until break m = do
+    res <- m
+    if break res then pure res else until break m
+
 run :: IO ()
-run = putStrLn "Running Exy..."
+run = do
+    _ <- until (== "quit") $ do
+            putStrLn "Type something..."
+            getLine
+    pure ()
+
