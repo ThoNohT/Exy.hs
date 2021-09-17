a13c4265c2516f028c9556abbd82b8dddb8fc1e3
ThoNohT <e.c.p.bataille@gmail.com>
2021-09-17 15:15:11 +0200

    Start of parser stuff

--- src/Exy.hs
+++ src/Exy.hs
@@ -4,9 +4,32 @@ import Control.Monad.State.Lazy
 import System.IO
 import Prelude hiding (until)
 
+-- Type alias for a function that can parse a value of type a.
+-- Returns either an error string, or the parsed value combined with the remaining string.
+type Parser a = String -> Either String (a, String)
+
+-- Type class for a type that can be parsed.
+class Parse a where
+  parse :: Parser a
+
+pChar :: Parser Char
+pChar [] = Left "No more input"
+pChar (x : xs) = Right (x, xs)
+
+pCheck :: (a -> Bool) -> Parser a -> Parser a
+pCheck check parser input =
+  let pResult = parser input
+   in case pResult of
+        Left err -> Left err
+        Right (p, rem) -> if check p then pResult else Left "Check on parse result failed"
+
+data Statement
+  = Set String String
+  | Get String
+
 data Output = Continue | Quit deriving (Eq)
 
-type ExyState = [ String ]
+type ExyState = [String]
 
 run :: IO ()
 run = run' []
