bac9a8e89159f260f995c8729a12c0e37775fd36
ThoNohT <e.c.p.bataille@gmail.com>
2021-09-18 18:06:22 +0200

    Some parsers with parsec

--- src/Exy.hs
+++ src/Exy.hs
@@ -2,13 +2,9 @@ module Exy (run) where
 
 import Control.Monad.State.Lazy
 import System.IO
+import Text.Parsec (alphaNum, anyChar, char, digit, letter, many1, (<|>), string)
+import Text.Parsec.Text.Lazy (Parser)
 import Prelude hiding (until)
-
--- | The different types of tokens available for building commands.
-data Token
-  = Keyword String
-  | Variable Variable
-  | Literal Literal
 
 newtype Variable = MkVariable String
 
@@ -18,6 +14,40 @@ data Literal = NumLiteral Integer | StringLiteral String
 data Statement
   = Set Variable Literal
   | Get Variable
+
+variableParser :: Parser Variable
+variableParser = do
+  firstChar <- letter
+  otherChars <- many1 variableChar
+  pure $ MkVariable (firstChar : otherChars)
+  where
+    variableChar = alphaNum <|> char '_'
+
+numLiteral = do
+  nums <- many1 digit
+  pure $ NumLiteral $ read nums
+
+stringLiteral = do
+  chars <- char '"' *> many1 anyChar <* char '\"'
+  pure $ StringLiteral chars
+
+literalParser :: Parser Literal
+literalParser = numLiteral <|> stringLiteral
+
+
+setStatementParser :: Parser Statement
+setStatementParser = do
+  _ <- string "set"
+  var <- variableParser
+  Set var <$> literalParser
+
+getStatementParser :: Parser Statement
+getStatementParser = do
+  _ <- string "get"
+  Get <$> variableParser
+
+statementParser :: Parser Statement
+statementParser = getStatementParser <|> setStatementParser
 
 data Output = Continue | Quit deriving (Eq)
 
