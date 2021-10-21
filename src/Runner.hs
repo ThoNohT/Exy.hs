69435a869d6a05069fd30811052d396dfd011f84
ThoNohT <e.c.p.bataille@gmail.com>
2021-10-21 14:28:11 +0200

    Refactor runner code to allow easy showing of declaration after storing

--- src/Runner.hs
+++ src/Runner.hs
@@ -4,10 +4,11 @@ import Control.Monad (foldM_)
 import Control.Monad.State.Lazy (MonadIO (liftIO), MonadState (get, put), StateT (runStateT))
 import Data.Map.Strict as Map
 import qualified Data.Text as T
-import Exy (Declaration (..), ExyState, Output (..), Statement (..), createDeclaration, showExpr)
+import Exy (Declaration (..), ExyState, Output (..), Statement (..), createDeclaration, showExpr, Variable (Variable), Expression)
 import Lexer (lexText)
 import Parser (Parser, end, runParser, statement)
 import Text.Printf (printf)
+import Control.Monad.RWS.Lazy (modify)
 
 -- | Runs the Exy update loop.
 run :: IO ()
@@ -16,6 +17,30 @@ run = run' Map.empty
     run' st = do
       (out, st') <- runStateT step st
       if out == Quit then pure () else run' st'
+
+-- | Displays all information about a variable, given the provided state.
+showVar :: Variable -> ExyState -> IO ()
+showVar var state = do
+  putStrLn $ printf "===== [ %s ] =====" (showExpr var)
+  let val = Map.lookup var state
+  case val of
+    Nothing -> liftIO $ putStrLn $ printf "Variable declaration '%s' not found" (show var)
+    Just decl -> do
+      liftIO $ putStrLn $ printf "Expression: %s" (showExpr (declExpr decl))
+      liftIO $ case declType decl of
+        Left err -> putStrLn $ printf "Type Error: %s" err
+        Right t -> putStrLn $ printf "Type: %s" $ show t
+
+-- | Stores a variable with an expression in the state.
+-- Overrides any older declaration for this variable.
+storeVar :: Variable -> Expression -> ExyState -> ExyState
+storeVar var expr state = do
+  let decl = createDeclaration state expr
+  Map.insert var decl state
+
+-- | Removes a variable from the state, has no effect if the variable did not exist.
+clearVar :: Variable -> ExyState -> ExyState
+clearVar = Map.delete
 
 -- | A single step in the Exy update loop.
 step :: StateT ExyState IO Output
@@ -29,26 +54,18 @@ step = do
     Right (stmt, _) -> do
       case stmt of
         Load var -> do
-          oldState <- get
-          let val = Map.lookup var oldState
-          case val of
-            Nothing -> liftIO $ putStrLn $ printf "Variable declaration '%s' not found" (show var)
-            Just decl -> do
-              liftIO $ putStrLn $ printf "Expression: %s" (showExpr (declExpr decl))
-              liftIO $ case declType decl of
-                Left err -> putStrLn $ printf "Type Error: %s" err
-                Right t -> putStrLn $ printf "Type: %s" $ show t
+          get >>= liftIO . showVar var
         Store var expr -> do
-          oldState <- get
-          let decl = createDeclaration oldState expr
-          put $ Map.insert var decl oldState
-          liftIO $ putStrLn "Stored"
+          state <- get
+          let newState = storeVar var expr state
+          put newState
+          liftIO $ showVar var newState
         Clear var -> do
-          oldState <- get
-          put $ Map.delete var oldState
+          modify (clearVar var)
           liftIO $ putStrLn "Cleared"
     Left err -> liftIO $ putStrLn $ T.unpack err
 
   case input of
     "quit" -> pure Quit
     _ -> pure Continue
+
